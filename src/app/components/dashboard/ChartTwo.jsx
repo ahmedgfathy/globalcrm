80e82a7677732b7e8ace1b13c44e2394ffac3709
Omar Sherif <91194829+shefoo-om@users.noreply.github.com>
2024-11-03 05:56:23 +0000

    finished the dashboard cards

---
+++ src/app/components/dashboard/ChartTwo.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from 'react';
+import { BarChart, Bar, ResponsiveContainer, Tooltip } from 'recharts';
+
+const data = [
+    { name: 'Page A', uv: 40, pv: 20, amt: 40 },
+    { name: 'Page B', uv: 30, pv: 19, amt: 20 },
+    { name: 'Page c', uv: 70, pv: 90, amt: 22 },
+    { name: 'Page D', uv: 60, pv: 90, amt: 22 },
+    { name: 'Page E', uv: 40, pv: 90, amt: 22 },
+    { name: 'Page F', uv: 20, pv: 90, amt: 22 },
+    { name: 'Page G', uv: 30, pv: 90, amt: 22 },
+];
+
+const ChartComponentTwo = () => {
+    return (
+        <ResponsiveContainer width={70} height={60}>
+            <BarChart width={10} height={140} data={data}>
+                <Bar dataKey="uv" fill="#007867" />
+            </BarChart>
+            <Tooltip />
+        </ResponsiveContainer>
+    );
+};
+
+export default ChartComponentTwo;
