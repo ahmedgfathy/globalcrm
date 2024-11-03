c572f5cc4e33d0b9ed36c1095874fd4c1c5cf832
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2024-11-03 17:59:49 +0000

    response-menu

--- src/app/components/nav-bar/NavBar.jsx
+++ src/app/components/nav-bar/NavBar.jsx
@@ -1,16 +1,15 @@
 "use client";
 import React from "react";
 import Drawer from "../Drawer/Drawer";
-import SideBar from "../side-bar/SideBar";
 import Menu from "../menu/Menu";
 import { usePathname } from "next/navigation";
 
 function NavBar() {
   const pathName = usePathname();
   return (
-    <nav className="fixed top-0 left-0 right-0" dir="ltr">
+    <nav className="fixed top-0 left-0 right-0 menu-drawer 10" dir="ltr">
       <div className="container mx-auto">
-        <div className="grid grid-cols-12 items-center h-20 justify-items-center">
+        <div className="grid grid-cols-12 items-center h-10 justify-items-center">
           {pathName.split("/").includes("dashboard") && (
             <div className="avatar lg:col-span-1 max-sm:col-span-2">
               <Menu />
@@ -19,9 +18,6 @@ function NavBar() {
           <div className="setting lg:col-span-1 max-sm:col-span-2 ">
             <Drawer />
           </div>
-          {/* <div className="side-bar lg:col-span-10 max-sm:col-span-8 text-end">
-            <SideBar />
-          </div> */}
         </div>
       </div>
     </nav>
--- src/app/components/side-bar/SideBar.jsx
+++ src/app/components/side-bar/SideBar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { links } from "./data";
 import styles from "./SideBar.module.css";
 import Link from "next/link";
@@ -7,52 +7,89 @@ import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { IoArrowBackCircleOutline } from "react-icons/io5";
 import { useTranslation } from "@/app/context/TranslationContext";
-function SideBar() {
-  const { t } = useTranslation();
+import { useIsMobile } from "@/hooks/use-mobile";
+import { CiMenuFries } from "react-icons/ci";
 
-  const [isOpen, setIsOpen] = useState(false);
+function SideBar() {
+  const isMobile = useIsMobile();
+  const { t } = useTranslation();
+  const [isOpen, setIsOpen] = useState(!isMobile);
   const pathName = usePathname();
+  const sidebarRef = useRef(null);
+
   const isActive = (href) => {
     if (pathName.split("/").includes(href)) return true;
     if (pathName === "/dashboard" && href === "/") return true;
   };
+
+  useEffect(() => {
+    setIsOpen(!isMobile);
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (isMobile) {
+      const handleClickOutside = (event) => {
+        if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+          setIsOpen(false);
+        }
+      };
+
+      document.addEventListener("mousedown", handleClickOutside);
+
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }
+  }, [isMobile]);
+
   return (
     <div className="relative z-50">
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        data-drawer-target="sidebar-multi-level-sidebar"
-        data-drawer-toggle="sidebar-multi-level-sidebar"
-        aria-controls="sidebar-multi-level-sidebar"
-        type="button"
-        className="absolute z-40 top-5 left-0 -translate-x-1/2 flex justify-center items-center bg-white w-10 h-10 rounded-full text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-      >
-        <span className="sr-only">Open sidebar</span>
-        <IoArrowBackCircleOutline className="text-2xl" />
-      </button>
-      <aside
-        id="sidebar-multi-level-sidebar"
-        className={`${styles.sidebar} ${
-          isOpen ? styles.sidebarOpen : styles.sidebarClosed
-        } h-screen bg-white dark:bg-gray-800`}
-        aria-label="Sidebar"
-      >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-white dark:bg-gray-800 border-x shadow-sm">
-          <ul className="space-y-2 font-medium">
-            <li className="logo">
-              <Image
-                width={50}
-                height={50}
-                alt="logo"
-                src="/assets/logo/logo.webp"
-                className="mx-auto"
-              />
-            </li>
+      {isMobile ? (
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          aria-controls="sidebar-multi-level-sidebar"
+          type="button"
+          className="absolute duration-200 z-40 top-5 -left-5 -translate-x-1/2 flex justify-center items-center w-10 h-10 text-sm text-gray-500  "
+        >
+          <span className="sr-only">Open sidebar</span>
+          <CiMenuFries className="text-2xl" />
+        </button>
+      ) : (
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          aria-controls="sidebar-multi-level-sidebar"
+          type="button"
+          className="absolute duration-200 z-40 top-5 left-0 -translate-x-1/2 flex justify-center items-center bg-white dark:bg-gray-600 w-10 h-10 rounded-full text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        >
+          <span className="sr-only">Open sidebar</span>
+          <IoArrowBackCircleOutline className="text-2xl" />
+        </button>
+      )}
 
-            {links.map((item) => {
-              return (
+      {(isOpen || !isMobile) && (
+        <aside
+          ref={sidebarRef}
+          id="sidebar-multi-level-sidebar"
+          className={`${styles.sidebar} ${
+            isOpen ? styles.sidebarOpen : styles.sidebarClosed
+          } h-screen bg-white dark:bg-gray-800`}
+          aria-label="Sidebar"
+        >
+          <div className="h-full px-3 py-4 overflow-y-auto bg-white dark:bg-dark border-x shadow-sm">
+            <ul className="space-y-2 font-medium">
+              <li className="logo">
+                <Image
+                  width={50}
+                  height={50}
+                  alt="logo"
+                  src="/assets/logo/logo.webp"
+                  className="mx-auto"
+                />
+              </li>
+
+              {links.map((item) => (
                 <li key={item.id}>
                   <Link
-                    type="button"
                     href={`/dashboard/${item.link}`}
                     className={`flex items-center w-full gap-3 ${
                       isOpen
@@ -60,8 +97,8 @@ function SideBar() {
                         : "flex-col justify-center items-center text-base font-medium"
                     } ${
                       isActive(item.link) &&
-                      "bg-tea_green text-tea_green-200 hover:text-tea_green-100  hover:bg-tea_green-600 dark:bg-light_green dark:text-tea_green-100"
-                    }  p-2 text-gray-900 transition duration-75 rounded-lg group dark:hover:text-tea_green-100 hover:bg-gray-100 dark:text-white dark:hover:bg-tea_green-700`}
+                      "bg-tea_green text-tea_green-200 hover:text-tea_green-100 hover:bg-tea_green-600 dark:bg-light_green dark:text-tea_green-100"
+                    } p-2 text-gray-900 transition duration-75 rounded-lg group dark:hover:text-tea_green-100 hover:bg-gray-100 dark:text-white dark:hover:bg-tea_green-700`}
                   >
                     <span>{item.icon()}</span>
                     <span className="flex-1 text-left rtl:text-right whitespace-nowrap">
@@ -69,11 +106,11 @@ function SideBar() {
                     </span>
                   </Link>
                 </li>
-              );
-            })}
-          </ul>
-        </div>
-      </aside>
+              ))}
+            </ul>
+          </div>
+        </aside>
+      )}
     </div>
   );
 }
