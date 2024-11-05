e71044ac87f972ff2bfa21d9d0c2a10415c75b5f
joker-0-7 <jokere136@gmail.com>
2024-11-05 05:33:18 +0200

    user-pagev1

---
+++ src/app/components/user-components/Details.jsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React from "react";
+
+function Details() {
+  return (
+    <Card className="menu-drawer w-full h-[90vh]">
+      <CardHeader>
+        <div className="header flex justify-between items-center">
+          <CardTitle>Lead Details</CardTitle>
+          <div className="buttons">
+            <Button className="mx-3">Update</Button>
+            <Button variant="destructive">Delete</Button>
+          </div>
+        </div>
+        <CardDescription>
+          Make changes to your account here. Click save when you&apos;re done.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" defaultValue="Pedro Duarte" />
+        </div>
+        <div className="space-y-1">
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" defaultValue="@peduarte" />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button>Save changes</Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default Details;
--- tailwind.config.js
+++ tailwind.config.js
@@ -277,11 +277,19 @@ module.exports = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      }, boxShadow: {
-        box_shadow: "rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px"
-      },screens:{
-        "1400":"1400px"
-      }
+      },
+      boxShadow: {
+        box_shadow:
+          "rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px",
+      },
+      screens: {
+        1400: "1400px",
+      },
+    },
+    variants: {
+      extend: {
+        backgroundColor: ["aria-selected"],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
