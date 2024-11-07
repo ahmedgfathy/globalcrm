aee03c118f7949eea3560d62351b461e661c0d8e
AmiraAli1612001 <programming.tarcks.gmail.com>
2024-11-07 13:58:02 +0200

    fix

--- src/app/dashboard/units/page.jsx
+++ src/app/dashboard/units/page.jsx
@@ -1,13 +1,17 @@
 "use client";
-import { Box, Grid } from '@mui/material';
-import React from 'react';
-import UnitsTable from "../../../components/ui/tables";
-import { useTranslation } from '@/app/context/TranslationContext';
-import Link from 'next/link';
-import { AiOutlineUserAdd } from 'react-icons/ai';
-import { FaWhatsapp } from 'react-icons/fa6';
-import { MdOutlineMail } from 'react-icons/md';
-import { FiPhoneCall } from 'react-icons/fi'; 
+import { useTranslation } from "@/app/context/TranslationContext";
+import { Button } from "@/components/ui/button";
+import { Grid } from "@mui/material";
+import Link from "next/link";
+import React from "react";
+import {
+  AiOutlineUserAdd,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
+import { FaWhatsapp } from "react-icons/fa";
+import { FiPhoneCall } from "react-icons/fi";
+import { MdOutlineMail } from "react-icons/md";
+
 // import imgg from "@/puplic/assets/images/default-user.jpg"
 
 const ClientDetails = [
@@ -18,6 +22,28 @@ const ClientDetails = [
 export default function Page() {
   const { t } = useTranslation();  
 
+  const ClientDetails = [
+    { id: 1, name: "Ahmed", phone: "01011001105", href: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=600" },
+  { id: 2, name: "ALi", phone: "01011424232", href: "https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=600" },
+  { id: 3, name: "Khalid", phone: "01014303103", href: "https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=600" },
+  { id: 4, name: "Khalid", phone: "01014303103", href: "https://images.pexels.com/photos/280222/pexels-photo-280222.jpeg?auto=compress&cs=tinysrgb&w=600" },
+  { id: 5, name: "Khalid", phone: "01014303103", href: "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=600" },
+  { id: 6, name: "Khalid", phone: "01014303103", href: "https://images.pexels.com/photos/1396132/pexels-photo-1396132.jpeg?auto=compress&cs=tinysrgb&w=600" },
+  { id: 7, name: "Khalid", phone: "01014303103", href: "https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=600" },
+  { id: 8, name: "Khalid", phone: "01014303103", href: "https://images.unsplash.com/photo-1416331108676-a22ccb276e35?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHJlYWwlMjBzdGF0ZXxlbnwwfHwwfHx8MA%3D%3D" },
+  { id: 9, name: "Khalid", phone: "01014303103", href: "https://images.unsplash.com/photo-1484154218962-a197022b5858?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmVhbCUyMHN0YXRlfGVufDB8fDB8fHww" },
+  { id: 10, name: "Khalid", phone: "01014303103", href: "https://media.istockphoto.com/id/1899637807/photo/cityscape-of-a-residential-area-with-modern-apartment-buildings-new-sustainable-urban.webp?a=1&b=1&s=612x612&w=0&k=20&c=il_mc160U9aObhGQRfbZyUSqPOxzpX4F9ATaR9HQ7_c=" },
+  { id: 11, name: "Khalid", phone: "01014303103", href: "https://plus.unsplash.com/premium_photo-1684508638760-72ad80c0055f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aG9tZXxlbnwwfHwwfHx8MA%3D%3D" },
+  { id: 12, name: "Khalid", phone: "01014303103", href: "https://images.unsplash.com/photo-1501183638710-841dd1904471?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvbWV8ZW58MHx8MHx8fDA%3D" },
+  { id: 13, name: "Khalid", phone: "01014303103", href: "https://media.istockphoto.com/id/1288886616/photo/modern-apartment-building.webp?a=1&b=1&s=612x612&w=0&k=20&c=K0dOBLItQAhXHNiL-Ch2jcbMr1cmoDOvYyqRqWTJ_nw=" },
+  { id: 14, name: "Khalid", phone: "01014303103", href: "https://media.gettyimages.com/id/1149958175/photo/modern-skyscrapers-in-midtown-manhattan.jpg?s=612x612&w=0&k=20&c=Q8y5mYKJKp2vm4g7VQsfTWKK6XdoLjVrXR4yBcNX724=" },
+  { id: 15, name: "Khalid", phone: "01014303103", href: "https://media.gettyimages.com/id/1437086785/photo/multi-color-leaves-in-neighbourhood-park-at-weston-road-and-major-mackenzie-dr-woodbridge.jpg?s=612x612&w=0&k=20&c=1t0OVT0dmjCVZQgnBBD6qLXpN9vBlhC92hL-dHKZTFQ=" },
+  { id: 16, name: "Khalid", phone: "01014303103", href: "https://media.gettyimages.com/id/1321598038/photo/modern-luxury-holiday-villa-at-seaside.jpg?s=612x612&w=0&k=20&c=teTOgGgaQaL3Et0-vUcFTKcGR5HegeBQjPfPC5Xo_o8=" },
+  { id: 17, name: "Khalid", phone: "01014303103", href: "https://media.gettyimages.com/id/1435149042/photo/cityscape-of-a-residential-area-with-modern-apartment-buildings.jpg?s=612x612&w=0&k=20&c=ueetSwQs9VJ_qFpKOKaoDF8_tEEQcYDTGv43WD5HKyc=" },
+  { id: 18, name: "Khalid", phone: "01014303103", href: "https://media.gettyimages.com/id/1990444472/photo/scandinavian-style-cozy-living-room-interior.jpg?s=612x612&w=0&k=20&c=qgzrs_4vKDrOVo6gVc0EVb9-PziE-REbV9DcM5ZAfig=" },
+  { id: 19, name: "Khalid", phone: "01014303103", href: "https://media.gettyimages.com/id/1421422160/photo/interior-of-living-room.jpg?s=612x612&w=0&k=20&c=r8Hyrk-1JtHSJS8TA5BfostSuIEd6-L2fLYMoyEBf_E=" },
+  { id: 20, name: "Khalid", phone: "01014303103", href: "https://media.gettyimages.com/id/1409181133/photo/large-home-exterior-nevada.jpg?s=612x612&w=0&k=20&c=BzNy8vfsAwfrRF_X9qhigJJUYTMqbcMEuA62LPGwi5o=" },
+  ]
   return (
     <div className="py-2">
       <Grid container className="p-6 w-full">
@@ -43,7 +69,7 @@ export default function Page() {
           <Grid item xs={12} sm={5.7} lg={2.8} key={ele.id}>
             <div className="rounded-t max-h-max hover:shadow duration-300 bg-Lightbg dark:bg-cardbgDark flex flex-col justify-between items-center gap-4 rounded-b">
               <div className="overflow-hidden w-full lg:h-[200px] sm:h-[230px]">
-              <img className="rounded-t block bg-cover w-full h-full hover:scale-105 duration-500" src="/assets/images/default-user.jpg" alt="User Image" />
+              <img className="rounded-t block bg-cover w-full h-full hover:scale-105 duration-500" src={ele.href} alt="User Image" />
 
                 {/* <img
                   className="rounded-t block bg-cover w-full h-full hover:scale-105 duration-500"
