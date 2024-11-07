76cf91e0ed68656d96dce41409eab5fbafef4735
Omar Sherif <91194829+shefoo-om@users.noreply.github.com>
2024-11-07 09:09:49 +0000

    create the date filed

--- src/app/components/user-components/utils/DateInput.jsx
+++ src/app/components/user-components/utils/DateInput.jsx
@@ -2,13 +2,17 @@ import { Label } from '@/components/ui/label'
 import { DatePopover } from '../../Date-Filed/DatePopover';
 import React from 'react'
 
-function DateFiled({ label, id, defaultValue, isDisabled }) {
+function DateInput({ label, id, defaultValue, isDisabled }) {
     return (
         <div className="space-y-2">
             <Label htmlFor={id}>{label}</Label>
-            <DatePopover defaultValue={defaultValue} />
+            <DatePopover
+                defaultValue={defaultValue}
+                isDisabled={isDisabled}
+                id={id}
+            />
         </div>
     )
 }
 
-export default DateFiled;
+export default DateInput;
--- src/app/components/user-components/utils/FormFields.jsx
+++ src/app/components/user-components/utils/FormFields.jsx
@@ -3,60 +3,60 @@ import InputField from './InputField';
 import { Label } from '@/components/ui/label';
 import ClintFollowUp from './ClintFollowUp';
 import TextAreaField from './TextAreaField';
-
+import DateInput from './DateInput'
 function FormFields({ isDisabled, t }) {
-    const data = [
-        { id: 1, value: "ahmed", label: "Ahmed" },
-        { id: 2, value: "mohamed", label: "Mohamed" },
-        { id: 3, value: "ali", label: "Ali" },
-      ];
-    
-      return (
-        <div className="space-y-4 lg:col-span-3">
-          <div className="grid gap-4 sm:grid-cols-2">
-            <InputField
-              label={t("name_client")}
-              id="name"
-              defaultValue="Pedro Duarte"
-              isDisabled={isDisabled}
-            />
-            <InputField
-              label={t("lead_number")}
-              id="username"
-              defaultValue="@peduarte"
-              isDisabled={isDisabled}
-            />
-            <InputField
-              label={t("Number")}
-              id="number"
-              defaultValue="1111"
-              isDisabled={isDisabled}
-            />
-            <InputField
-              label={t("Last_Follow_up")}
-              id="LastFollowUp"
-              defaultValue="30-08-2022"
-              isDisabled={isDisabled}
-            />
-            <TextAreaField
-              label={t("descriptions")}
-              id="descriptions"
-              defaultValue="..."
-              isDisabled={isDisabled}
-            />
-            <div className="space-y-2 flex flex-col justify-center">
-              <Label htmlFor="clintFollowUp">{t("Client_follow_up")}</Label>
-              <ClintFollowUp data={data} isDisabled={isDisabled} />
-            </div>
-            <InputField
-              label={t("class")}
-              id="classType"
-              defaultValue="A"
-              isDisabled={isDisabled}
-            />
-          </div>
+  const data = [
+    { id: 1, value: "ahmed", label: "Ahmed" },
+    { id: 2, value: "mohamed", label: "Mohamed" },
+    { id: 3, value: "ali", label: "Ali" },
+  ];
+
+  return (
+    <div className="space-y-4 lg:col-span-3">
+      <div className="grid gap-4 sm:grid-cols-2">
+        <InputField
+          label={t("name_client")}
+          id="name"
+          defaultValue="Pedro Duarte"
+          isDisabled={isDisabled}
+        />
+        <InputField
+          label={t("lead_number")}
+          id="username"
+          defaultValue="@peduarte"
+          isDisabled={isDisabled}
+        />
+        <InputField
+          label={t("Number")}
+          id="number"
+          defaultValue="1111"
+          isDisabled={isDisabled}
+        />
+        <DateInput
+          label={t("Last_Follow_up")}
+          id="LastFollowUp"
+          defaultValue="2022-08-30"
+          isDisabled={isDisabled}
+        />
+        <TextAreaField
+          label={t("descriptions")}
+          id="descriptions"
+          defaultValue="..."
+          isDisabled={isDisabled}
+        />
+        <div className="space-y-2 flex flex-col justify-center">
+          <Label htmlFor="clintFollowUp">{t("Client_follow_up")}</Label>
+          <ClintFollowUp data={data} isDisabled={isDisabled} />
         </div>
-        )
+        <InputField
+          label={t("class")}
+          id="classType"
+          defaultValue="A"
+          isDisabled={isDisabled}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default FormFields
