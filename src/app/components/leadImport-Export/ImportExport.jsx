ea6649285b69112b725b6dcc1af9156d99774fba
omar sherif <gadoomarsh@gmail.com>
2024-11-14 11:09:52 +0200

    update

--- src/app/components/leadImport-Export/ImportExport.jsx
+++ src/app/components/leadImport-Export/ImportExport.jsx
@@ -19,13 +19,15 @@ export function DropdownMenImportExport() {
             <DropdownMenuTrigger asChild>
                 <Button className="bg-transparent border-none" variant="outline"><EllipsisVertical /></Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56 bg-gray-100 dark:bg-gray-900">
+            <DropdownMenuContent className="w-max bg-gray-100 dark:bg-gray-900">
                 <DropdownMenuGroup dir="rtl">
-                    <DropdownMenuItem className="hover:bg-transparent">
-                        <CustomButton title={t("import")} fun={() => alert("Importing...")} icon={() => <FaFileImport />} className="max-sm:w-1/3 GreenButton" />
+                    <DropdownMenuItem className="hover:bg-transparent text-bold">
+                        {/* <CustomButton title={t("import")} fun={() => alert("Importing...")} icon={() => <FaFileImport />} className="max-sm:w-1/3 GreenButton" /> */}
+                        {t("Import")}
                     </DropdownMenuItem>
-                    <DropdownMenuItem className="hover:bg-transparent">
-                        <CustomButton title={t("export")} fun={() => alert("Exporting...")} icon={() => <FaFileExport />} className="max-sm:w-1/3 GreenButton" />
+                    <DropdownMenuItem className="hover:bg-transparent text-bold">
+                    {t("Export")}
+                        {/* <CustomButton title={t("export")} fun={() => alert("Exporting...")} icon={() => <FaFileExport />} className="max-sm:w-1/3 GreenButton" /> */}
                     </DropdownMenuItem>
                 </DropdownMenuGroup>
             </DropdownMenuContent>
