19d1f63dc062a06887c3cef58bc44b95800c7932
joker-0-7 <jokere136gmail.com>
2024-12-07 21:50:21 +0200

    handle images

--- src/app/(pages)/units/add-unit/page.jsx
+++ src/app/(pages)/units/add-unit/page.jsx
@@ -60,24 +60,35 @@ const router = useRouter();
   };
 
   const handleImageChange = async (event) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
+    const files = event.target.files;
+    if (files.length > 0) {
+      const newImages = [];
+      const newFiles = [];
+  
+      for (const file of files) {
+        const reader = new FileReader();
         reader.onload = (e) => {
-        setImages((prevImages) => [...prevImages, e.target.result]);
-      };
-      reader.readAsDataURL(file);
-        setImagesFile((prevFiles) => [...prevFiles, file]);
+          setImages((prevImages) => [...prevImages, e.target.result]);
+        };
+        reader.readAsDataURL(file);
+  
+        newFiles.push(file);
         try {
-        const response = await uploadPropertyImages(file);
-        console.log(response)
-        setUnit((prevLead) => ({ ...prevLead, propertyImage: [...prevLead.propertyImage, response.fileUrl] }));
-        console.log("Image uploaded successfully:", response);
-      } catch (error) {
-        console.error("Error uploading image:", error);
+          const response = await uploadPropertyImages(file);
+          setUnit((prevLead) => ({
+            ...prevLead,
+            propertyImage: [...prevLead.propertyImage, response.fileUrl],
+          }));
+          console.log("Image uploaded successfully:", response);
+        } catch (error) {
+          console.error("Error uploading image:", error);
+        }
       }
+  
+      setImagesFile((prevFiles) => [...prevFiles, ...newFiles]);
     }
   };
+  
 
   const handleDeleteImage = async (index, id) => {
     console.log(index, id)
