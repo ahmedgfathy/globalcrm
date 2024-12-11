15e77b6c855d4d2342aed1abefdafc5f2e3fa590
joker-0-7 <jokere136gmail.com>
2024-12-12 01:07:35 +0200

    handle7

--- src/actions/propertiesAction.js
+++ src/actions/propertiesAction.js
@@ -661,7 +661,7 @@ export const exportProperties = async () => {
         [
           Query.limit(limit),
           Query.offset(offset),
-          Query.orderDesc('$createdAt') 
+          Query.orderDesc('$createdAt')
         ]
       )
 
